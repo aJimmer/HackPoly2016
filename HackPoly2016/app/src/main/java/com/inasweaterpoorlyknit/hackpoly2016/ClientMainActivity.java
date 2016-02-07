dea0d84f20bb732b58c26710c5cd988778d8403c
Ezekiel24r <ericrensel@yahoo.com>
2016-02-07 03:25:09 -0800

    ClientMainActivity

--- HackPoly2016/app/src/main/java/com/inasweaterpoorlyknit/hackpoly2016/ClientMainActivity.java
+++ HackPoly2016/app/src/main/java/com/inasweaterpoorlyknit/hackpoly2016/ClientMainActivity.java
@@ -1,11 +1,13 @@
 package com.inasweaterpoorlyknit.hackpoly2016;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -18,12 +20,43 @@ public class ClientMainActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.songrequest";
     public String songRequest;
     Button sendRequest;
+    Button connectToHost;
+    SharedPreferences prefs;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //initialize shared pref. folder to save host date
+        /*prefs = getSharedPreferences("HostData", MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("host_ip", "null");
+        editor.apply();
+
+
+
+
+        TextView hostInfo = (TextView) findViewById(R.id.hostDisplay);
+        hostInfo.setText(prefs.getString("host_ip", "none set"));*/
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_main);
+        //trying to get code to work on enter key
+        /*procRequest = (EditText) findViewById(R.id.songBox);
 
         sendRequest = (Button)findViewById(R.id.sendRequest);
+
+        connectToHost = (Button) findViewById(R.id.connectToHost);
+
+        connectToHost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), HostList.class);
+                startActivity(intent);
+
+
+
+            }
+        });*/
+
+        sendRequest = (Button) findViewById(R.id.sendRequest);
 
         sendRequest.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,16 +79,16 @@ public class ClientMainActivity extends AppCompatActivity {
     }
 
     public void sendMessage(String str){
-        Scanner userInput = new Scanner(System.in);
-        String message;
+        //Scanner userInput = new Scanner(System.in);
+        //String message;
         Socket socket = null;
         try {
-            socket = new Socket("192.168.40.119", 9000);
-            message = userInput.nextLine();
+            socket = new Socket("192.168.43.87", 9000);
+            //message = userInput.nextLine();
             OutputStream os = socket.getOutputStream();
             PrintStream out = new PrintStream(os);
-            System.out.println("Client >> " + message);
-            out.println(message);
+            //System.out.println("Client >> " + str);
+            out.println(str);
             socket.close();
         } catch (IOException e) {
             e.printStackTrace();
