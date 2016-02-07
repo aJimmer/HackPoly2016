9cf749388dc03871f595fb4d52faca99a79e8dd7
RaymondArias <rarias2010@yahoo.com>
2016-02-06 16:30:50 -0800

    API Key

--- HackPoly2016/app/src/main/java/com/inasweaterpoorlyknit/hackpoly2016/HostMainActivity.java
+++ HackPoly2016/app/src/main/java/com/inasweaterpoorlyknit/hackpoly2016/HostMainActivity.java
@@ -7,12 +7,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class HostMainActivity extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+
+public class HostMainActivity extends AppCompatActivity implements YouTubePlayer.OnInitializedListener{
+    public static String API_Key = "AIzaSyCOX5sjc9q9FK3eHwBipWqNR1WRfr7maUw";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_host__lobby);
+
+
     }
 
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+    }
 }
