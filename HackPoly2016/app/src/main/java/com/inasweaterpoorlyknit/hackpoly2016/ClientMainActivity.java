cd82c477ef74ce6e9bb71d6170be50ecc819ba95
Connor Haskins <ConnorAHaskins@gmail.com>
2016-02-06 18:42:11 -0800

    Adding the search activity to the project. Only in prototype mode right now.

--- HackPoly2016/app/src/main/java/com/inasweaterpoorlyknit/hackpoly2016/ClientMainActivity.java
+++ HackPoly2016/app/src/main/java/com/inasweaterpoorlyknit/hackpoly2016/ClientMainActivity.java
@@ -1,13 +1,32 @@
 package com.inasweaterpoorlyknit.hackpoly2016;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ClientMainActivity extends AppCompatActivity {
+    private static final int SEARCH_CODE = 2;
+    String foundSongID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_main);
+
+        final FloatingActionButton searchButton = (FloatingActionButton) findViewById(R.id.search_button);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                searchSong();
+            }
+        });
+    }
+
+    public void searchSong() {
+        Intent searchIntent = new Intent(this, SearchActivity.class);
+        searchIntent.putExtra("foundSong", foundSongID);
+        startActivityForResult(searchIntent, SEARCH_CODE);
     }
 }
