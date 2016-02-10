1acc6075821aa3cf2151d896dddaf87da77fe710
RaymondArias <rarias2010@yahoo.com>
2016-02-09 18:00:13 -0800

    Rebase head from github

--- HackPoly2016/app/src/main/java/com/inasweaterpoorlyknit/hackpoly2016/ClientMainActivity.java
+++ HackPoly2016/app/src/main/java/com/inasweaterpoorlyknit/hackpoly2016/ClientMainActivity.java
@@ -1,22 +1,19 @@
 package com.inasweaterpoorlyknit.hackpoly2016;
 
 import android.content.Intent;
-import android.support.design.widget.FloatingActionButton;
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import com.google.android.youtube.player.YouTubePlayerFragment;
-
 import java.io.IOException;
-import java.net.Socket;
-import java.util.Scanner;
-import java.io.PrintStream;
 import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.Socket;
 
 public class ClientMainActivity extends AppCompatActivity {
     
