f57f35c3ac7033241baa1e5b821040ae315bbf79
Lucodivo <chaskins483@student.msjc.edu>
2016-03-16 08:05:05 -0700

    Added comments

--- HackPoly2016/app/src/main/java/com/inasweaterpoorlyknit/hackpoly2016/PlaylistAdapter.java
+++ HackPoly2016/app/src/main/java/com/inasweaterpoorlyknit/hackpoly2016/PlaylistAdapter.java
@@ -16,57 +16,66 @@ import java.io.InputStream;
 import java.util.ArrayList;
 
 /**
- * Created by Connor on 3/5/2016.
+ * Displays a list of youtube video titles and thumbnails
+ * An adapter used for a listview in PlaylistFragment and SearchFragment
+ * [does NOT currently keep track of song ids]
  */
 public class PlaylistAdapter extends ArrayAdapter<String> {
-    private final Activity context;
-    private final ArrayList<String> songTitles;
-    private final ArrayList<String> thumbnailURLs;
+    private final Activity context; // activity the fragment was called from
+    
+    private final ArrayList<String> songTitles; // pointer to an ArrayList of songTitles that exists in the activity
+    private final ArrayList<String> thumbnailURLs; // pointer to an ArrayList of songThumbnails that exists in the activity
 
+    // public constructor
     public PlaylistAdapter(Activity context, ArrayList<String> songTitles, ArrayList<String> thumbnailURLs){
         super(context, R.layout.song_row, songTitles);
-        this.context = context;
-        this.songTitles = songTitles;
-        this.thumbnailURLs = thumbnailURLs;
+        
+        this.context = context; // set the context to the activity that called it
+        this.songTitles = songTitles; // set the member songTitles to the passed songTitles
+        this.thumbnailURLs = thumbnailURLs; // set the member thumbnailURLs to the passed thumbnailURLs
     }
 
+    
     @Override
     public View getView(int position, View view, ViewGroup parent){
         LayoutInflater inflater = context.getLayoutInflater();
         View rowView = inflater.inflate(R.layout.song_row, null, true);
 
-        if(thumbnailURLs != null) {
-            TextView songTitle = (TextView) rowView.findViewById(R.id.song_title);
-            songTitle.setText(songTitles.get(position));
+        if(thumbnailURLs != null) { // if there are thumbnail URLs(and any songs on the playlist)
+            TextView songTitle = (TextView) rowView.findViewById(R.id.song_title); // get access to the row's song_title
+            songTitle.setText(songTitles.get(position));    // set the text of the row's song_title to the song title at correct position
             new DownloadImageTask((ImageView) rowView.findViewById(R.id.video_thumbnail))
-                    .execute(thumbnailURLs.get(position));
+                    .execute(thumbnailURLs.get(position));  // download the thumbnail for that song and set as bitmap for the imageview
         }
-
-        return rowView;
+        return rowView; // return the rowView that was created
     }
 
+    // AsyncTask to download the thumbnail images
     private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
-        ImageView bmImage;
+        ImageView bmImage;  // image view to add the image to
 
+        // publiv constructor
         public DownloadImageTask(ImageView bmImage) {
-            this.bmImage = bmImage;
+            this.bmImage = bmImage; // initialize the member ImageView to the one passed 
         }
 
+        // downloads any number of URLs in the background
         protected Bitmap doInBackground(String... urls) {
-            String urlDisplay = urls[0];
-            Bitmap mIcon11 = null;
+            String urlDisplay = urls[0];    // save the first url
+            Bitmap thumbnail = null;          // thumbnail, set to null
             try {
-                InputStream in = new java.net.URL(urlDisplay).openStream();
-                mIcon11 = BitmapFactory.decodeStream(in);
-            } catch (Exception e) {
+                InputStream in = new java.net.URL(urlDisplay).openStream(); // get an input stream from specified url
+                thumbnail = BitmapFactory.decodeStream(in);   // decode the inputStream as a Bitmap
+            } catch (Exception e) { // printe any errors
                 Log.e("Error", e.getMessage());
                 e.printStackTrace();
             }
-            return mIcon11;
+            return thumbnail;   // return the thumbnail
         }
 
+        // called after bitmap is loaded and returned from doInBackground()
         protected void onPostExecute(Bitmap result) {
-            bmImage.setImageBitmap(result);
+            bmImage.setImageBitmap(result); // set the bitmap image in the bitmap view
         }
     }
 }
